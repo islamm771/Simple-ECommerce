445880aeadceaf725322727fbdd5f84eaa2c2a84
islamm771 <90573842+islamm771@users.noreply.github.com>
2024-11-06 02:03:57 +0200

    modify dashboard home

--- src/pages/Dashboard/Index.tsx
+++ src/pages/Dashboard/Index.tsx
@@ -1,5 +1,8 @@
+import { FaUser } from "react-icons/fa";
 import { getUserData } from "../../data";
 import useAuthenticatedQuery from "../../hooks/useAuthenticatedQuery"
+import { BiSolidCategory } from "react-icons/bi";
+import { MdProductionQuantityLimits } from "react-icons/md";
 
 const Dashboard = () => {
     const user = getUserData();
@@ -14,16 +17,25 @@ const Dashboard = () => {
     })
     return (
         <div className="grid lg:grid-cols-3 gap-5">
-            <div className="p-4 shadow-md rounded-md">
-                <h3 className="text-xl mb-3">Users</h3>
+            <div className="p-4 pt-6 pb-8 shadow-md rounded-md">
+                <h3 className="text-xl mb-3 flex items-center gap-2">
+                    <FaUser />
+                    Users
+                </h3>
                 <p>Total users: {data?.counts.users}</p>
             </div>
-            <div className="p-4 shadow-md rounded-md">
-                <h3 className="text-xl mb-3">Product</h3>
-                <p>Total product: {data?.counts.products}</p>
+            <div className="p-4 pt-6 pb-8 shadow-md rounded-md">
+                <h3 className="text-xl mb-3 flex items-center gap-2">
+                    <MdProductionQuantityLimits size={23} />
+                    Products
+                </h3>
+                <p>Total products: {data?.counts.products}</p>
             </div>
-            <div className="p-4 shadow-md rounded-md">
-                <h3 className="text-xl mb-3">Categories</h3>
+            <div className="p-4 pt-6 pb-8 shadow-md rounded-md">
+                <h3 className="text-xl mb-3 flex items-center gap-2">
+                    <BiSolidCategory size={25} />
+                    Categories
+                </h3>
                 <p>Total categories: {data?.counts.categories}</p>
             </div>
         </div>
